ee276af85b30d448465298da3c44a3ac34728513
z4hid <zahidhasan9297@gmail.com>
2024-06-12 16:16:16 +0600

    Cart configuration & templates updated

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,6 @@
 # cart/urls.py
 from django.urls import path
-from .views import cart, checkout, add_to_cart
+from .views import cart, checkout, add_to_cart, hx_menu_cart, update_cart, hx_cart_total
 
 app_name = 'cart'
 
@@ -8,4 +8,7 @@ urlpatterns = [
     path('', cart, name='cart'),
     path('checkout/', checkout, name='checkout'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('cart/update_cart/<int:product_id>/<str:action>/', update_cart, name='update_cart'),
+    path('hx_menu_cart/', hx_menu_cart, name='hx_menu_cart'),
+    path('hx_cart_total/', hx_cart_total, name='hx_cart_total'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .cart import Cart
+from product.models import Product
+
+from django.http import JsonResponse
 
 def add_to_cart(request, product_id):
     cart = Cart(request)
@@ -9,8 +12,60 @@ def add_to_cart(request, product_id):
     return render(request, 'cart/menu_cart.html')
 
 def cart(request):
+    # cart = Cart(request)
+    # print(cart)
+    # for item in cart:
+    #     print(item)
     return render(request, 'cart/cart.html')
+
+def update_cart(request, product_id, action):
+    try:
+        cart = Cart(request)
+        
+        if action == 'increment':
+            cart.add(product_id, 1, True)
+        elif action == 'decrement':
+            cart.add(product_id, -1, True)
+        else:
+            return JsonResponse({'error': 'Invalid action'}, status=400)
+
+        product = get_object_or_404(Product, pk=product_id)
+
+        cart_item = cart.get_item(product_id)
+        if not cart_item:
+            return JsonResponse({'error': 'Cart item not found'}, status=404)
+
+        quantity = cart_item['quantity']
+
+        item = {
+            'product': {
+                'id': product.id,
+                'name': product.name,
+                'slug': product.slug,
+                'image': product.image.url,
+                'get_thumbnail': product.get_thumbnail(),
+                'price': product.price,
+            },
+            'total_price': product.price * quantity,
+            'quantity': quantity
+        }
+
+        response = render(request, 'cart/partials/cart_item.html', {'item': item})
+        response['HX-Trigger'] = 'update-menu-cart' 
+        return response
+
+    except Product.DoesNotExist:
+        return JsonResponse({'error': 'Product does not exist'}, status=404)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
 
 @login_required
 def checkout(request):
     return render(request, 'cart/checkout.html')
+
+def hx_menu_cart(request):
+    return render(request, 'cart/partials/menu_cart.html')
+
+def hx_cart_total(request):
+    return render(request, 'cart/partials/cart_total.html')
