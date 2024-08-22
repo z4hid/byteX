985a77d41ade07d9e92c04bf7561d47bd45f9fd0
z4hid <zahidhasan9297@gmail.com>
2024-08-23 02:55:25 +0600

    context processors  updated

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -2,4 +2,13 @@ from .cart import Cart
 
 
 def cart(request):
+    """
+    Returns a dictionary containing the user's cart.
+    
+    Args:
+        request (HttpRequest): The HTTP request object.
+    
+    Returns:
+        dict: A dictionary with a single key 'cart' containing the user's Cart instance.
+    """
     return {'cart': Cart(request)}
